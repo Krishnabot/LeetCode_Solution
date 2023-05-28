cb7c7fd32f148a776cb1a28abd10b3301c8ac077
Krishna <libraryismyparadise@gmail.com>
2023-05-28 21:31:25 +0545

    Correct the string_to_integer function

--- string_to_integer.rb
+++ string_to_integer.rb
@@ -1,31 +1,36 @@
 def my_atoi(s)
-    # Remove leading whitespace
-    s = s.lstrip
+    s = s.strip  # Remove leading and trailing whitespace
+  
+    # Check if the string is empty
+    return 0 if s.empty?
   
     # Initialize variables
     sign = 1
     result = 0
+    i = 0
   
     # Check for sign character
     if s[0] == '-' || s[0] == '+'
       sign = s[0] == '-' ? -1 : 1
-      s = s[1..-1]
+      i += 1
     end
   
     # Iterate through digits
-    s.each_char do |char|
-      break unless char.match?(/\d/)  # Break if a non-digit character is encountered
-      result = result * 10 + char.to_i
+    while i < s.length && s[i].match?(/\d/)
+      digit = s[i].to_i
+  
+      # Check for overflow
+      if result > (2**31 - 1 - digit) / 10
+        return sign == 1 ? 2**31 - 1 : -2**31
+      end
+  
+      result = result * 10 + digit
+      i += 1
     end
   
-    # Clamp the result to the 32-bit signed integer range
-    result = [[-2**31, result].max, 2**31 - 1].min
-  
-    # Apply the sign
-    result *= sign
-  
-    result
+    result * sign
   end
+  
 
   
 puts my_atoi("42")                # Output: 42
