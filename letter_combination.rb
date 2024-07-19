a10df360d728e0bc4eaa78625731e1d842156d07
Krishna <libraryismyparadise@gmail.com>
2024-07-20 01:33:28 +0900

    Improve runtime of letter_combination

--- letter_combination.rb
+++ letter_combination.rb
@@ -64,8 +64,35 @@ def letter_combinations_2(digits)
 
   result
 end
-p letter_combinations_2("23") # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+# p letter_combinations_2("23") # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 
 
+#Attempt 3
+#Instad of creating a new array new_result in each itration use map!
+
+
+def letter_combinations_3(digits)
+  return [] if digits.empty?
+  digit_to_letter_map = {
+    '2' => 'abc',
+    '3' => 'def',
+    '4' => 'ghi',
+    '5' => 'jkl',
+    '6' => 'mno',
+    '7' => 'pqrs',
+    '8' => 'tuv',
+    '9' => 'wxyz'
+  }
+  result = ['']
+
+  digits.each_char do |digit|
+    next if digit == '1' 
+
+    result = result.product(digit_to_letter_map[digit].chars).map(&:join)
+  end
+
+  result
+end
+p letter_combinations_3("23") # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 
 
