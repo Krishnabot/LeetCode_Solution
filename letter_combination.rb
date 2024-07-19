75f373e278a765095dc093639062452e10dc0fce
Krishna <libraryismyparadise@gmail.com>
2024-07-20 01:29:12 +0900

    Make  letter combiantion solution more  efficient using DFS

--- letter_combination.rb
+++ letter_combination.rb
@@ -1,4 +1,6 @@
-def letter_combinations(digits)
+#Attempt 1
+
+def letter_combinations_1(digits)
   return [] if digits.empty?
 
   digit_to_letter_map = {
@@ -28,4 +30,42 @@ def letter_combinations(digits)
   queue
 end
 
-p letter_combinations("23") # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+# p letter_combinations_1("23") # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+#Attempt 2
+
+
+
+def letter_combinations_2(digits)
+  return [] if digits.empty?
+  mapping = {
+    '2' => 'abc',
+    '3' => 'def',
+    '4' => 'ghi',
+    '5' => 'jkl',
+    '6' => 'mno',
+    '7' => 'pqrs',
+    '8' => 'tuv',
+    '9' => 'wxyz'
+  }
+  result = ['']
+
+  digits.each_char do |digit|
+    next if digit == '1' 
+
+    new_result = []
+    result.each do |prefix|
+      mapping[digit].each_char do |letter|
+        new_result << prefix + letter
+      end
+    end
+    result = new_result
+  end
+
+  result
+end
+p letter_combinations_2("23") # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+
+
+
