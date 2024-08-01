6521dc120664b08537aff80e664ad9d8c1a193ff
Krishna <libraryismyparadise@gmail.com>
2024-08-01 21:37:10 +0900

    Pseudo code generate paranthesis

---
+++ generate_paranthesis.rb
@@ -0,0 +1,15 @@
+function generate_parenthesis(n):
+    result = empty list
+    backtrack(result, "", 0, 0, n)
+    return result
+
+function backtrack(result, current, open, close, max):
+    if length of current is equal to max * 2:
+        add current to result
+        return
+
+    if open < max:
+        backtrack(result, current + "(", open + 1, close, max)
+
+    if close < open:
+        backtrack(result, current + ")", open, close + 1, max)
