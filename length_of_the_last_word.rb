c49bc216ea68be380c4f1c1ec25e049f15f96656
Krishna <libraryismyparadise@gmail.com>
2024-08-05 23:05:23 +0900

    Solve lebgth of the last word without inbuilt function

--- length_of_the_last_word.rb
+++ length_of_the_last_word.rb
@@ -1,8 +1,22 @@
-#using Inbuilt functions
 def length_of_last_word(s)
-  # Split the string into an array
-  words = s.split
-  
-  # Return length of the last word
-  words.last.length
+  length = 0
+  in_word = false
+
+  # Traverse the string from the end to the beginning
+  (s.length - 1).downto(0) do |i|
+    if s[i] != ' '
+      length += 1
+      in_word = true
+    elsif in_word
+      break
+    end
+  end
+
+  length
 end
+
+
+# Test Cases:
+puts length_of_last_word("Hello World") # Output: 5
+puts length_of_last_word("   fly me   to   the moon  ") # Output: 4
+puts length_of_last_word("luffy is still joyboy") # Output: 6
