f2ac1ec71542286386a614ed6348dfa9e4c98eda
Krishna <libraryismyparadise@gmail.com>
2024-08-06 21:45:03 +0900

    Optimise Add binary

--- add_binary.rb
+++ add_binary.rb
@@ -2,19 +2,19 @@ def add_binary(a, b)
   i = a.length - 1
   j = b.length - 1
   carry = 0
-  result = ""
+  result = []
 
   while i >= 0 || j >= 0 || carry != 0
     sum = carry
     sum += a[i].to_i if i >= 0
     sum += b[j].to_i if j >= 0
-    result = (sum % 2).to_s + result
+    result << (sum % 2).to_s
     carry = sum / 2
     i -= 1
     j -= 1
   end
 
-  result
+  result.reverse.join
 end
 
 # Test:
